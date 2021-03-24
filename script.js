9644e0a82ad17e7b6314cf0b1b80df22da6a7a67
Sylvain Viole <sylvainviole@gmail.com>
2021-03-24 10:34:43 +0100

    fix DATA typo, add SCRIPT.JS

---
+++ script.js
@@ -0,0 +1,25 @@
+// import questions
+const questions = require('./questions.js')
+
+// Dom elements
+const btnValid = document.querySelector('#btn-validation');
+const btnReset = document.querySelector('#btn-reset');
+const divScore = document.querySelector('#score-container');
+const answerDiv = document.querySelector('.answer-container');
+const answerRadioBtn = document.querySelectorAll('.answerRadioBtn');
+
+// 
+let answerArray = []
+
+function getAnswers() {
+    answerDiv.forEach(div => {
+        let divIndex = div.getAttribute('data-index')
+        answerRadioBtn.forEach(btn => {
+            if(btn.checked) {
+                answerArray[divIndex] = btn.getAttribute('id')
+            }
+        })        
+    });
+}
+
+
